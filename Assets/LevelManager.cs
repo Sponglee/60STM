9e4c898c76e27b75a2cc9ad93bd172e405ac1118
SolidHinken <solidhinken@gmail.com>
2019-07-22 01:15:07 +0300

    22.07.19
    levelGenerator

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -6,6 +6,10 @@ public class LevelManager : MonoBehaviour
 {
     public GameObject nodePref;
     public GameObject tilePref;
+    public GameObject exitPref;
+
+    public Transform exitsHolder;
+    public List<Transform> exits;
     public float nodeStep = 10.5f;
     
     public int levelDimention = 3;
@@ -13,22 +17,57 @@ public class LevelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        exits = new List<Transform>();
+
+
         for (int i = 0; i < levelDimention; i++)
         {
             for (int j = 0; j < levelDimention; j++)
             {
                 GameObject tmpNode = Instantiate(nodePref, new Vector3(nodeStep*j-nodeStep * (levelDimention / 2), 0, -nodeStep * i + nodeStep*(levelDimention/2)), Quaternion.identity, transform);
-                if(Random.Range(0,100)>30)
+
+                tmpNode.GetComponent<NodeController>().Row = i;
+                tmpNode.GetComponent<NodeController>().Column = j;
+
+
+                if((i== levelDimention/2 && j == levelDimention/2) || Random.Range(0,100)>40)
                 {
-                    Instantiate(tilePref,tmpNode.transform));
+                    GameObject tmpTile = Instantiate(tilePref,tmpNode.transform.position,  Quaternion.Euler(0,Random.Range(0,360)/90*90,0), tmpNode.transform);
+
+
+                    //If center - enable rocket and disable buildings
+                    if(i == levelDimention/2 && j== levelDimention/2)
+                    {
+                        tmpTile.transform.GetChild(1).gameObject.SetActive(true);
+                        tmpTile.transform.GetChild(2).gameObject.SetActive(false);
+                        //Enable road layout
+                        tmpTile.transform.GetChild(0).GetChild(0).gameObject.SetActive(true);
+                        
+                    }
+                    else
+                    {
+                        //Generate building layout
+                        int buildingIndex = Random.Range(0, tmpTile.transform.GetChild(2).childCount);
+                        tmpTile.transform.GetChild(2).GetChild(buildingIndex).gameObject.SetActive(true);
+                        //Generate road layout
+                        int roadIndex = Random.Range(0, tmpTile.transform.GetChild(0).childCount);
+                        tmpTile.transform.GetChild(0).GetChild(roadIndex).gameObject.SetActive(true);
+
+                    }
+                }
+                else
+                {
+                    if(exits.Count== 0 || Random.Range(0,100)<10)
+                    {
+
+                        GameObject tmpTile = Instantiate(exitPref, tmpNode.transform.position, Quaternion.Euler(0, Random.Range(0, 360) / 90 * 90, 0), tmpNode.transform);
+                        exits.Add(tmpTile.transform);
+                    }
                 }
             }
         }
+
+     
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/NodeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeController : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private int column;
+    public int Column { get => column; set => column = value; }
+
+    [SerializeField]
+    private int row;
+    public int Row { get => row; set => row = value; }
+}
