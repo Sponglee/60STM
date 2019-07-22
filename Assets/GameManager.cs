e8d85bf88c8eebbbcd35bbf062871c95ff3ed568
SolidHinken <solidhinken@gmail.com>
2019-07-22 12:01:45 +0300

    22.07.19
    TileRotate system

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,4 +5,18 @@ using UnityEngine;
 public class GameManager : Singleton<GameManager>
 {
    public Transform selectedTile;
+
+
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0) && selectedTile != null)
+        {
+            selectedTile.GetComponent<TileManager>().CollidedBool = false;
+            selectedTile.GetComponent<TileManager>().Selected = false;
+            selectedTile.GetComponent<TileManager>().DragActive = false;
+            selectedTile = null;
+        }
+    }
 }
--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class TileManager : MonoBehaviour
 {
     [SerializeField]
-    private bool CollidedBool = false;
+    private bool collidedBool = false;
+    public bool CollidedBool { get => collidedBool; set => collidedBool = value; }
     [SerializeField]
-    private bool Selected = false;
+    private bool selected = false;
+    public bool Selected { get => selected; set => selected = value; }
+    [SerializeField]
+    private bool dragActive = false;
+    public bool DragActive { get => dragActive; set => dragActive = value; }
 
+    [SerializeField]
+    private bool RotationInProgress = false;
+
+    [SerializeField]
+    private Animator tileAnim;
 
     private Vector3 screenPoint;
     private Vector3 offset;
 
     private Vector3 oldPosition;
 
+  
     void OnMouseDown()
     {
         Selected = true;
+        GameManager.Instance.selectedTile = transform;
+
+
         oldPosition = transform.parent.position;
 
         screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
@@ -26,23 +40,55 @@ public class TileManager : MonoBehaviour
 
     }
 
+
+    private void OnMouseUp()
+    {
+        if (!RotationInProgress && !DragActive)
+            StartCoroutine(StopRotate());
+      
+    }
+
+
     void OnMouseDrag()
     {
         if(!CollidedBool)
         {
+            
             Vector3 cursorScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
             Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorScreenPoint) + offset;
             transform.position = new Vector3(cursorPosition.x, transform.position.y, cursorPosition.z);
         }
       
     }
+    public IEnumerator StopRotate()
+    {
+        RotationInProgress = true;
+        float angle = 90f;
+        float duration = 0.5f;
+        float elapsed = 0;
+        Vector3 startEul = transform.eulerAngles;
+        Vector3 destEul = startEul + new Vector3(0, angle, 0);
+        Debug.Log("HH");
+        while (Mathf.Abs(transform.eulerAngles.y - destEul.y%360f) >= 0.05f)
+        {
+            transform.eulerAngles = Vector3.Lerp(startEul, destEul, elapsed / duration);
+            Debug.Log(">R> " + transform.eulerAngles + " " + destEul.y%360f);
+            elapsed += Time.deltaTime;
 
+            yield return null;
+        }
+        RotationInProgress = false;
+    }
+
+
+  
 
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Empty") && !CollidedBool && Selected)
         {
+            DragActive = true;
             CollidedBool = true;
             Debug.Log("HERE");
             //Remember tmp parent and position
@@ -66,12 +112,5 @@ public class TileManager : MonoBehaviour
     }
 
 
-    private void Update()
-    {
-        if(Input.GetMouseButtonUp(0))
-        {
-            CollidedBool = false;
-            Selected = false;
-        }
-    }
+  
 }
